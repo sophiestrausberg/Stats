09bfe68c52dd5507f94a6b817c75a64479371818
Sophie Strausberg <sophie.strausberg@gmail.com>
2022-04-18 20:27:31 -0400

    Initial Commit

---
+++ NewStatistics/NewStatisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewStatisticsApp.swift
+//  NewStatistics
+//
+//  Created by Sophie Strausberg on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NewStatisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
